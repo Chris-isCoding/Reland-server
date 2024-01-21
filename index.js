17177d55084e39c3112d8e53d000640fafbd65ce
Chris K. <Chris-SourceCode@outlook.com>
2024-01-21 01:11:17 +0100

    fix: Remove / from domain

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT ?? 5001;
 
-app.use(cors({ origin: 'https://reland-client.vercel.app/' }));
+app.use(cors({ origin: 'https://reland-client.vercel.app' }));
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello World!' });
