f89e46ce1ea33682aee6cf366131a4da263494d9
Chris K. <Chris-SourceCode@outlook.com>
2024-01-21 01:03:01 +0100

    refactor: Change domain for cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT ?? 5001;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://reland-client.vercel.app/' }));
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello World!' });
